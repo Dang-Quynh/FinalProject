4a9f3c3cfacb8b7329427dbeb4af0077ce0c275d
quynhdt8 <Hoadv220696*>
2024-03-23 22:03:44 +0700

    update final project

---
+++ src/main/java/pages/ClientPage.java
@@ -0,0 +1,27 @@
+package pages;
+import org.openqa.selenium.WebDriver;
+
+public class ClientPage {
+    private WebDriver driver;
+
+    public ClientPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // auth = quynh
+    public void filterById(){
+
+    }
+
+
+    // auth = duy
+    public void searchBy(){
+
+    }
+
+
+    // auth = chau
+    public void addClient(){
+
+    }
+}
