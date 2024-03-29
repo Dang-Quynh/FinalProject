8ed13c92a8f985dacb81dd774d6756abcb93e5bd
QuynhDT39 <Quynhdt39@fpt.com.vn>
2024-03-29 14:58:37 +0700

    update code

--- src/main/java/constant/CT_Common.java
+++ src/main/java/constant/CT_Common.java
@@ -6,7 +6,10 @@ public class CT_Common {
     public static String URL_rise_signin = "https://rise.fairsketch.com/signin";
 
     // filter
+    public static By PAGINATION = By.xpath("//ul[@class='pagination']");
     public static By TASK_MENU = By.xpath("//ul[@id='sidebar-menu']//a//span[text()='Tasks']");
     public static By RELATED_TO_VALUE_COLUMNS = By.xpath("//table[@id='task-table']//tr//td[6]//a");
+    public static By MILESTONE_VALUE_COLUMNS = By.xpath("//table[@id='task-table']//tr//td[5]");
+    public static By ASSIGNED_TO_VALUE_COLUMNS = By.xpath("//table[@id='task-table']//tr//td[7]");
 
 }
--- src/main/java/pages/TaskPage.java
+++ src/main/java/pages/TaskPage.java
@@ -26,10 +26,16 @@ public class TaskPage {
     @FindBy(id = "s2id_autogen6_search")
     WebElement inputSearchRelatedToFilter;
 
-    @FindBy(id = "")
+    @FindBy(id = "s2id_autogen7")
     WebElement dropdownProjectFilter;
-    @FindBy(id = "")
+    @FindBy(id = "s2id_autogen8_search")
+    WebElement inputSearchProjectFilter;
+    @FindBy(id = "s2id_autogen9")
     WebElement dropdownMilestoneFilter;
+    @FindBy(xpath = "//ul[@id='select2-results-10']//li[3]")
+    WebElement valueMilestoneFilter_release;
+    @FindBy(xpath = "//ul[@id='select2-results-10']//li[2]")
+    WebElement valueMilestoneFilter_beta_release;
     @FindBy(id = "s2id_autogen11")
     WebElement dropdownTeamMemberFilter;
     @FindBy(id = "s2id_autogen12_search")
@@ -43,6 +49,38 @@ public class TaskPage {
     @FindBy(id = "")
     WebElement dropdownStatusFilter;
 
+    @FindBy(xpath = "//a[text()=' Add task']")
+    WebElement addTaskBtn;
+    @FindBy(xpath = "//input[@placeholder='Title']")
+    WebElement addTask_titleInput;
+    @FindBy(id = "s2id_task-context")
+    WebElement addTask_RelateToDropdown;
+    @FindBy(xpath = "//div[@id='select2-drop']//input")
+    WebElement addTask_SelectSearchInput;
+    @FindBy(id = "s2id_project_id")
+    WebElement addTask_ProjectDropdown;
+    @FindBy(id = "s2id_client_id")
+    WebElement addTask_ClientDropdown;
+    @FindBy(id = "s2id_contract_id")
+    WebElement addTask_ContractDropdown;
+    @FindBy(id = "s2id_estimate_id")
+    WebElement addTask_EstimateDropdown;
+    @FindBy(id = "s2id_expense_id")
+    WebElement addTask_ExpenseDropdown;
+    @FindBy(id = "s2id_invoice_id")
+    WebElement addTask_InvoiceDropdown;
+    @FindBy(id = "s2id_lead_id")
+    WebElement addTask_LeadDropdown;
+    @FindBy(id = "s2id_order_id")
+    WebElement addTask_OrderDropdown;
+    @FindBy(id = "s2id_proposal_id")
+    WebElement addTask_ProposalDropdown;
+    @FindBy(id = "s2id_subscription_id")
+    WebElement addTask_SubscriptionDropdown;
+    @FindBy(id = "s2id_ticket_id")
+    WebElement addTask_TicketDropdown;
+    @FindBy(xpath = "//button[@type='submit' and text()=' Save']")
+    WebElement addTask_SaveBtn;
 
     public TaskPage(WebDriver driver) {
         this.driver = driver;
@@ -50,12 +88,74 @@ public class TaskPage {
     }
 
     // auth = quynh
-    private void waitTableLoadData(){
+    public void addTaskDataForRelatedToFilter(String RelatedTo) throws InterruptedException {
+        addTaskBtn.click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("task-table")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Title']")));
+        addTask_titleInput.sendKeys("addRelateToData");
+        addTask_RelateToDropdown.click();
+        addTask_SelectSearchInput.sendKeys(RelatedTo);
+        addTask_SelectSearchInput.sendKeys(Keys.TAB);
+        Thread.sleep(1000);
+
+        switch (RelatedTo){
+            case "Client":
+                addTask_ClientDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Contract":
+                addTask_ContractDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Estimate":
+                addTask_EstimateDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Expense":
+                addTask_ExpenseDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Invoice":
+                addTask_InvoiceDropdown.click();
+                addTask_SelectSearchInput.sendKeys("inv");
+                break;
+            case "Lead":
+                addTask_LeadDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Order":
+                addTask_OrderDropdown.click();
+                addTask_SelectSearchInput.sendKeys("order");
+                break;
+            case "Proposal":
+                addTask_ProposalDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Subscription":
+                addTask_SubscriptionDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            case "Ticket":
+                addTask_TicketDropdown.click();
+                addTask_SelectSearchInput.sendKeys("a");
+                break;
+            default:
+                addTask_ProjectDropdown.click();
+                break;
+        }
+
+        addTask_SelectSearchInput.sendKeys(Keys.TAB);
+        addTask_SaveBtn.click();
+        waitTableLoadData();
     }
 
-    public void filterByRelatedTo(String relatedTo){
+    private void waitTableLoadData() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("task-table")));
+        Thread.sleep(1000);
+    }
+
+    public void filterByRelatedTo(String relatedTo) throws InterruptedException {
         // show filter list
         showFilterButton.click();
 
@@ -67,20 +167,64 @@ public class TaskPage {
 
         // set default value for TeamMember filter
         dropdownTeamMemberFilter.click();
+        inputSearchTeamMemberFilter.sendKeys("Team member");
         inputSearchTeamMemberFilter.sendKeys(Keys.TAB);
         waitTableLoadData();
     }
 
-    public void filterByProject(){
+    public void filterByProject(String projectName) throws InterruptedException {
+        // show filter list
+        showFilterButton.click();
 
+        // senKey Related to
+        dropdownProjectFilter.click();
+        inputSearchProjectFilter.sendKeys(projectName);
+        inputSearchProjectFilter.sendKeys(Keys.TAB);
+        waitTableLoadData();
+
+        // set default value for TeamMember filter
+        dropdownTeamMemberFilter.click();
+        inputSearchTeamMemberFilter.sendKeys("Team member");
+        inputSearchTeamMemberFilter.sendKeys(Keys.TAB);
+        waitTableLoadData();
     }
 
-    public void filterByMilestone(){
+    public void filterByMilestone(String projectName, String milestone) throws InterruptedException {
+        // show filter list
+        showFilterButton.click();
 
+        // senKey Related to
+        dropdownProjectFilter.click();
+        inputSearchProjectFilter.sendKeys(projectName);
+        inputSearchProjectFilter.sendKeys(Keys.TAB);
+        waitTableLoadData();
+        Thread.sleep(1000);
+        // senKey Milestone
+        dropdownMilestoneFilter.click();
+        if(milestone == "Release"){
+            valueMilestoneFilter_release.click();
+        }
+        else {
+            valueMilestoneFilter_beta_release.click();
+        }
+        waitTableLoadData();
+
+        // set default value for TeamMember filter
+        dropdownTeamMemberFilter.click();
+        inputSearchTeamMemberFilter.sendKeys("Team member");
+        inputSearchTeamMemberFilter.sendKeys(Keys.TAB);
+        waitTableLoadData();
     }
 
-    public void filterByTeamMember(){
+    public void filterByTeamMember(String teamMember) throws InterruptedException {
+        // show filter list
+        showFilterButton.click();
 
+        // set default value for TeamMember filter
+        dropdownTeamMemberFilter.click();
+        inputSearchTeamMemberFilter.sendKeys(teamMember);
+        inputSearchTeamMemberFilter.sendKeys(Keys.TAB);
+        waitTableLoadData();
     }
 
     public void filterByPriority(){
